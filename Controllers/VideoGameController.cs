9a80146661707161693b079bb8828a1b5e018a5e
mobile-pablo <pawel.lukasz001@gmail.com>
2025-07-21 17:53:36 +0200

    Add .NET init files

---
+++ Controllers/VideoGameController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using VideoGameExample.Models;
+
+namespace VideoGameExample.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VideoGameController : ControllerBase
+{
+    static private List<VideoGame> videoGames = new List<VideoGame> {
+        new VideoGame
+        {
+            Id = 1, 
+            Title = "Spiderman 2",
+            Platform = "PC",
+            Developer = "Insomniac Games",
+            Publisher = "Sony Games Entertainment",
+        },
+        new VideoGame
+        {
+            Id = 2,
+            Title = "The Legend of Zelda : Breath of the wild",
+            Platform = "Nintendo Switch",
+            Developer = "Nintendo EPD",
+            Publisher = "Nintendo",
+        },
+        new VideoGame
+        {
+            Id = 3,
+            Title = "Cyberpunk 2077",
+            Platform = "PC",
+            Developer = "CD Project RED",
+            Publisher = "CD Project",
+        },
+        new VideoGame
+        {
+         Id   = 4,
+         Title = "Grand Theft Auto V",
+         Platform = "PC",
+         Developer = "Rockstar North",
+         Publisher = "Rockstar",
+        }
+    };
+
+    [HttpGet]
+    public ActionResult<List<VideoGame>> GetVideoGames()
+    {
+        return Ok(videoGames);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<VideoGame> GetVideoGame(int id)
+    {
+        VideoGame? videoGame = videoGames.FirstOrDefault(v => v.Id == id);
+        if(videoGame == null) return NotFound();
+        
+        return Ok(videoGame);
+    }
+}
---
+++ Models/VideoGame.cs
@@ -0,0 +1,10 @@
+namespace VideoGameExample.Models;
+
+public class VideoGame
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Platform { get; set; }
+    public string? Developer { get; set; }
+    public string? Publisher { get; set; }
+}
